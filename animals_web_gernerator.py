e88e7c898f215342fda2357fc033519213f4c9df
Robert <hemmoleg@gmail.com>
2025-03-24 15:12:56 +0100

    added more info to animals / better html for animal list

--- animals_web_gernerator.py
+++ animals_web_gernerator.py
@@ -14,21 +14,33 @@ def load_html(file_path):
 def create_animal_data_html(animal_data):
     animal_data_string = ""
 
-    for animal in animal_data:
-        animal_data_string += "<li class='cards__item'>"
-        animal_data_string += f"<div class='card__title'>Name: {animal['name']}</div>"
-        animal_data_string += "<p class='card__text'>"
+    for animal_obj in animal_data:
+        animal_data_string += serialize_animal(animal_obj)
 
-        if 'characteristics' in animal and 'diet' in animal['characteristics']:
-            animal_data_string += f"<strong>Diet:</strong> {animal['characteristics']['diet']}<br/>"
+    return animal_data_string
 
-        animal_data_string += f"<strong>Locations:</strong> {animal['locations'][0]}<br/>"
 
-        if 'characteristics' in animal and 'type' in animal['characteristics']:
-            animal_data_string += f"<strong>Type:</strong> {animal['characteristics']['type']}<br/>"
+def serialize_animal(animal_obj):
+    animal_data_string=""
+    animal_data_string += "<li class='cards__item'>"
+    animal_data_string += f"<div class='card__title'>Name: {animal_obj['name']}</div>"
+    animal_data_string += "<div class='card__text'><ul>"
 
-        animal_data_string += "</p></li>\n"
+    if 'characteristics' in animal_obj and 'diet' in animal_obj['characteristics']:
+        animal_data_string += f"<li><strong>Diet:</strong> {animal_obj['characteristics']['diet']}</li>"
 
+    animal_data_string += f"<li><strong>Locations:</strong> {animal_obj['locations'][0]}</li>"
+
+    if 'characteristics' in animal_obj and 'type' in animal_obj['characteristics']:
+        animal_data_string += f"<li><strong>Type:</strong> {animal_obj['characteristics']['type']}</li>"
+
+    if 'characteristics' in animal_obj and 'group_behavior' in animal_obj['characteristics']:
+        animal_data_string += f"<li><strong>Group behavior:</strong> {animal_obj['characteristics']['group_behavior']}</li>"
+
+    if 'characteristics' in animal_obj and 'top_speed' in animal_obj['characteristics']:
+        animal_data_string += f"<li><strong>Top speed:</strong> {animal_obj['characteristics']['top_speed']}</li>"
+
+    animal_data_string += "</ul></div></li>\n"
     return animal_data_string
 
 
