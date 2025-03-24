a17e779ccaf54bf4f1c20a784ab7afa2f49fba56
Robert <hemmoleg@gmail.com>
2025-03-24 14:36:32 +0100

    write animal data to html file

--- animals_web_gernerator.py
+++ animals_web_gernerator.py
@@ -5,17 +5,44 @@ def load_data(file_path):
   with open(file_path, "r") as handle:
     return json.load(handle)
 
-def main():
-    animal_data = load_data('animals_data.json')
+
+def load_html(file_path):
+    with open(file_path, "r") as handle:
+        return handle.read()
+
+
+def create_animal_data_string(animal_data):
+    animal_data_string = ""
 
     for animal in animal_data:
-        print(f"Name: {animal['name']}")
+        animal_data_string += f"Name: {animal['name']}\n"
+
         if 'characteristics' in animal and 'diet' in animal['characteristics']:
-            print(f"Diet: {animal['characteristics']['diet']}")
-        print(f"Locations {animal['locations'][0]}")
+            animal_data_string += f"Diet: {animal['characteristics']['diet']}\n"
+
+        animal_data_string += f"Locations {animal['locations'][0]}\n"
+
         if 'characteristics' in animal and 'type' in animal['characteristics']:
-            print(f"Type: {animal['characteristics']['type']}")
-        print()
+            animal_data_string += f"Type: {animal['characteristics']['type']}\n"
+
+        animal_data_string += "\n"
+
+    return animal_data_string
+
+
+def write_html(html):
+    with open('animal.html', "w") as handle:
+        handle.write(html)
+
+
+def main():
+    animal_data = load_data('animals_data.json')
+    html = load_html('animals_template.html')
+    animal_data_string = create_animal_data_string(animal_data)
+
+    html = str(html).replace("__REPLACE_ANIMALS_INFO__", animal_data_string)
+    write_html(html)
+
 
 if __name__ == '__main__':
     main()
