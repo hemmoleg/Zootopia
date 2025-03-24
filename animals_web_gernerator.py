7e871769c0376c724674ad2fed20b0c979257c45
Robert <hemmoleg@gmail.com>
2025-03-24 14:42:33 +0100

    generate card item for every animal

--- animals_web_gernerator.py
+++ animals_web_gernerator.py
@@ -11,21 +11,24 @@ def load_html(file_path):
         return handle.read()
 
 
-def create_animal_data_string(animal_data):
-    animal_data_string = ""
+def create_animal_data_html(animal_data):
+    animal_data_string = "<ul class='cards'>"
 
     for animal in animal_data:
-        animal_data_string += f"Name: {animal['name']}\n"
+        animal_data_string += "<li class='cards__item'>"
+        animal_data_string += f"Name: {animal['name']}<br/>\n"
 
         if 'characteristics' in animal and 'diet' in animal['characteristics']:
-            animal_data_string += f"Diet: {animal['characteristics']['diet']}\n"
+            animal_data_string += f"Diet: {animal['characteristics']['diet']}<br/>\n"
 
-        animal_data_string += f"Locations {animal['locations'][0]}\n"
+        animal_data_string += f"Locations {animal['locations'][0]}<br/>\n"
 
         if 'characteristics' in animal and 'type' in animal['characteristics']:
-            animal_data_string += f"Type: {animal['characteristics']['type']}\n"
+            animal_data_string += f"Type: {animal['characteristics']['type']}<br/>\n"
 
-        animal_data_string += "\n"
+        animal_data_string += "</li>\n"
+
+    animal_data_string += "</ul>"
 
     return animal_data_string
 
@@ -38,7 +41,7 @@ def write_html(html):
 def main():
     animal_data = load_data('animals_data.json')
     html = load_html('animals_template.html')
-    animal_data_string = create_animal_data_string(animal_data)
+    animal_data_string = create_animal_data_html(animal_data)
 
     html = str(html).replace("__REPLACE_ANIMALS_INFO__", animal_data_string)
     write_html(html)
